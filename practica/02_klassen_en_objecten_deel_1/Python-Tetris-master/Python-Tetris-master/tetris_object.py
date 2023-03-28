2ac6175193d4042e22c2d99007d356fd3d894425
ElmerHR <1069668@hr.nl>
2023-03-28 14:52:22 +0200

    Added tetris refactoring exercise

---
+++ practica/02_klassen_en_objecten_deel_1/Python-Tetris-master/Python-Tetris-master/tetris_object.py
@@ -0,0 +1,35 @@
+import pygame
+from random import randrange
+from copy import deepcopy
+
+W, H = 10, 20
+
+field = [[0 for i in range(W)] for j in range(H)]
+
+class Figure:
+    def __init__(self, shape):
+        self.shape = [pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in shape]
+    
+    def getShape(self):
+        return self.shape
+    
+    def getColor(self):
+        return (randrange(30, 256), randrange(30, 256), randrange(30, 256))
+    
+    def rotate(self, shape_old):
+        center = self.shape[0]
+        for i in range(4):
+            x = self.shape[i].y - center.y
+            y = self.shape[i].x - center.x
+            self.shape[i].x = center.x - x
+            self.shape[i].y = center.y + y
+            if not self.check_borders(i):
+                self.shape = deepcopy(shape_old.shape)
+                break
+    
+    def check_borders(self, i):
+        if self.shape[i].x < 0 or self.shape[i].x > W - 1:
+            return False
+        elif self.shape[i].y > H - 1 or field[self.shape[i].y][self.shape[i].x]:
+            return False
+        return True
