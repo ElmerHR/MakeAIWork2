927eea0ad3930006f86358b5fd3532d6429c0983
ElmerHR <1069668@hr.nl>
2023-04-04 12:17:00 +0200

    finalized naive implementation

--- practica/06_neurale_netwerken/naive_nn/naive_nn.py
+++ practica/06_neurale_netwerken/naive_nn/naive_nn.py
@@ -131,30 +131,90 @@ def cross_entropy_loss(yHat, y):
     return cel
 
 
-# def backpropagation():
+def update_weights_and_bias(W, B):
+    # generate random int between 0 and 9
+    r1 = np.random.randint(0, 9)
+    r2 = np.random.randint(0, 9)
+    # generate random float between 0 and 1
+    r_float1 = np.random.random_sample()
+    r_float2 = np.random.random_sample()
+    r_float3 = np.random.random_sample()*5
+    r_float4 = np.random.random_sample()*5
+    # substitute random index in weight and bias array with random float from above
+    W[0][r1] = r_float1
+    W[1][r2] = r_float2
+    B[0][0] = r_float3
+    B[0][1] = r_float4
+    return None
 
+def validate(x_test, yTest):
+    output = np.array([])
+    for input in x_test:
+        logits = []
+        # cast to numpy array
+        input = np.array(input["img"])
+        # flatten input to feed to nn
+        input = np.reshape(input, (1, -1))
+        # perform matrix multiplication
+        logits = np.matmul(input, W.T) + B
+        # calculate softmax probabilities
+        softmax_prob = softmax(logits)
+        if output.shape[0] == 0:
+            output = softmax_prob
+        else:
+            output = np.concatenate((output, softmax_prob), axis=0)
+    # ic(output)
+
+    cel = cross_entropy_loss(output, yTest)
+    ic(cel)
+    return None
 
 # initialize weights at 1
-W = np.stack((np.ones_like(np.reshape(np.array(training_data[0]["img"]), (1, -1)))[0], np.ones_like(np.reshape(np.array(training_data[0]["img"]), (1, -1)))[0]), axis=0)
-B = np.array([[0, 0]])
+W = np.stack((np.ones_like(np.reshape(np.array(training_data[0]["img"]), (1, -1)), dtype="float")[0], np.ones_like(np.reshape(np.array(training_data[0]["img"]), (1, -1)), dtype="float")[0]), axis=0)
+B = np.array([[0.0, 0.0]])
+# remember weights and bias
+rememberW = W.copy()
+rememberB = B.copy()
+# define y train data
 yTrain = np.array([y["label"] for y in training_data])
 
-output = np.array([])
-# loop over all training data
-for input in training_data:
-    logits = []
-    # cast to numpy array
-    input = np.array(input["img"])
-    # flatten input to feed to nn
-    input = np.reshape(input, (1, -1))
-    logits = np.matmul(input, W.T) + B
-    softmax_prob = softmax(logits)
-    if output.shape[0] == 0:
-        output = softmax_prob
+epochs = 1000
+cel_prev = 999999999999999
+for epoch in range(epochs):
+    print(f"####### EPOCH {epoch} ########")
+    output = np.array([])
+    # loop over all training data
+    for input in training_data:
+        logits = []
+        # cast to numpy array
+        input = np.array(input["img"])
+        # flatten input to feed to nn
+        input = np.reshape(input, (1, -1))
+        # perform matrix multiplication
+        logits = np.matmul(input, W.T) + B
+        # calculate softmax probabilities
+        softmax_prob = softmax(logits)
+        if output.shape[0] == 0:
+            output = softmax_prob
+        else:
+            output = np.concatenate((output, softmax_prob), axis=0)
+    # calculate cross entropy loss
+    cel = cross_entropy_loss(output, yTrain)
+    ic(cel, cel_prev)
+    # if cel is higher than previous cel than we don't want to remember the weight and bias changes and we reset them to the previous best weights and biases
+    if cel > cel_prev:
+        W = rememberW.copy()
+        B = rememberB.copy()
+    # if cel is lower, we want to update the prev cel as best cel and remember the weights and biases
     else:
-        output = np.concatenate((output, softmax_prob), axis=0)
-ic(output)
+        cel_prev = cel.copy()
+        ic(W, B)
+        rememberW = W.copy()
+        rememberB = B.copy()
+    # make new weights and biases
+    update_weights_and_bias(W, B)
 
-cel = cross_entropy_loss(output, yTrain)
-ic(cel)
-    
+# validate performance with test data
+yTest = np.array([y["label"] for y in test_data])
+print(f"####### VALIDATION ########")
+validate(test_data, yTest)
--- practica/06_neurale_netwerken/naive_nn/tryout.py
+++ practica/06_neurale_netwerken/naive_nn/tryout.py
@@ -1,12 +1,24 @@
 import numpy as np
 import math
-label = np.array([1, 0])
-y_hat = np.array([0.81, 0.19])
-cel = -np.sum(label * np.log(y_hat))
-print(cel)
+# # input array
+# x = np.array([[ 1., 2., 3.], [ 4., 5., 6.], [ 7., 8., 9.]])
 
-a = np.array([1, 2])
-b = np.array([5, 6])
+# # random boolean mask for which values will be changed
+# mask = np.random.randint(0,2,size=x.shape).astype(np.bool)
+# print(mask)
+# # random matrix the same shape of your data
+# r = np.random.rand(*x.shape)*np.max(x)
+# print(r)
+# # use your mask to replace values in your input array
+# x[mask] = r[mask]
+# print(x)
 
-c = np.concatenate((a,b), axis=0)
-print(c)
+x = np.array([[1., 1., 1., 1., 1., 1., 1., 1., 1.]])
+
+r = np.random.randint(0, 9)
+
+print(r)
+
+x[0][r] = 0.5
+
+print(x)
